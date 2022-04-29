e3868e6048299e6f97120387a6cf2bc4df40092f
fashiontechlab <fashiontechlabo@gmail.com>
2022-04-29 16:54:08 +0900

    画像のスライド機能の追加

--- js/script.js
+++ js/script.js
@@ -47,4 +47,51 @@ $(function(){
             }
         });
     });
+
+    window.onload = function(){
+
+        //画像を配列に格納
+        var images =['url(img/top_tanbo.jpg)',
+                     'url(img/top_take.jpg)',]
+
+        //要素をHTMLから取得
+        //画像
+        var target = document.getElementById('slide');
+        //>,<
+        var right = document.getElementById('right');
+        var left = document.getElementById('left');
+
+
+        //クリックしたときに変わるようにカウント変数
+        var count = 0;
+
+        //クリックしたら画像変更
+        function change(){
+            target.style.backgroundImage = images[count];
+        }
+
+        //>をクリックしたらcountを+1する
+        function goNext(){
+            if(count == 2){
+                count = 0;
+            }else{
+                count++;
+            }
+            change();
+        }
+
+        //<をクリックしたらcountを-1する
+        function goBack(){
+            if(count == 0){
+                count = 2;
+            }else{
+                count--;
+            }
+            change();
+        }
+
+        //クリックイベント
+        right.addEventListener('click',goNext,false);
+        left.addEventListener('click',goBack,false);
+    };
 });
